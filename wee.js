9730ad2794c2a353b1b375d1b8c4645c989a9573
Stanley Lin <slin55055@gmail.com>
2018-03-23 18:28:34 -0400

    typo fix

--- wee.js
+++ wee.js
@@ -17,10 +17,12 @@ function setAttr(elem, attrs) {
 		//gets the currently targeted element's value
 		let value = d3.event.target.value;
 		if (value === "2016") {
-			svg.selectAll(".bar").data(year2016);
+			console.log("2016");
+			graph.selectAll(".bar").data(year2016);
 		}
 		else if (value === "2017") {
-			svg.selectAll(".bar").data(year2017);
+			console.log("2017");
+			graph.selectAll(".bar").data(year2017);
 		}
 	});
 })();
